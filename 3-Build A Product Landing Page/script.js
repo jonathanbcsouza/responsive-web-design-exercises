1eb0b9cf3d497a78dd829c443082a8e682ba9303
Jonathan Souza <jonathanbcsouza@hotmail.com>
2021-04-06 01:01:24 +1200

    Refactor and update project 3

---
+++ 3-Build A Product Landing Page/script.js
@@ -0,0 +1,10 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav-bar").style.top = "0";
+  } else {
+    document.getElementById("nav-bar").style.top = "-50vh";
+  }
+  prevScrollpos = currentScrollPos;
+}
