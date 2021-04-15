b22d980aa96a1eb35ece4873fe0da7fe021b9cc7
Jonathan Souza <jonathanbcsouza@hotmail.com>
2021-04-16 02:15:04 +1200

    Refactor and update project 5

--- 5-Build A Personal Portfolio Webpage/scrypts/typewriter.js
+++ 5-Build A Personal Portfolio Webpage/scrypts/typewriter.js
@@ -1,43 +1,30 @@
-// JAVASCRIPT CODE GOT FROM https://codepen.io/danielgroen/pen/VeRPOq
+// JS code from https://codepen.io/danielgroen/pen/VeRPOq
 
 document.addEventListener('DOMContentLoaded', function (event) {
-  // array with texts to type in typewriter
-  var dataText = ["I would not just use things.", "I would not just copy.", "I would rather create them.", "and it is me", "Hello World"];
-  // type one text in the typwriter
-  // keeps calling itself until the text is finished
+  var dataText = ["Hi there..", "I am passionate about innovation...", "quality", "and people","Hello World."];
   function typeWriter(text, i, fnCallback) {
-    // chekc if text isn't finished yet
     if (i < (text.length)) {
-      // add next character to h1
       document.querySelector("h1").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
 
-      // wait for a while and call this function again for next character
       setTimeout(function () {
         typeWriter(text, i + 1, fnCallback)
       }, 100);
     }
-    // text finished, call callback if there is a callback function
     else if (typeof fnCallback == 'function') {
-      // call callback after timeout
       setTimeout(fnCallback, 700);
     }
   }
-  // start a typewriter animation for a text in the dataText array
   function StartTextAnimation(i) {
     if (typeof dataText[i] == 'undefined') {
       setTimeout(function () {
         StartTextAnimation(0);
       }, 20000);
     }
-    // check if dataText[i] exists
     if (i < dataText[i].length) {
-      // text exists! start typewriter animation
       typeWriter(dataText[i], 0, function () {
-        // after callback (and whole text has been animated), start next text
         StartTextAnimation(i + 1);
       });
     }
   }
-  // start the text animation
   StartTextAnimation(0);
 });
